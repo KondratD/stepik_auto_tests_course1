8351504fea2759a0e89dccca578c42a176c9229b
Kondrat <kondratda@kamaz.ru>
2026-09-09 13:58:28 +0300

    Убрал лишний импут и добавил редми

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# stepik_auto_tests_course1
+Создание виртуальное окружение:
+cd /home/user_name/repositories_name
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install selenium pytest
+pytest test_items.py
--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-import pytest
+import time
 from selenium.webdriver.common.by import By
 
 
@@ -7,6 +7,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_items(browser):
     browser.get(link)
+    time.sleep(5)
+
 
     button = browser.find_element(
         By.CSS_SELECTOR,
